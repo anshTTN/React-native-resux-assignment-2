502dcbd455b18de8492f986bdb069e89d1122956
Ansh Mittal <ansh.mittal@tothenew.com>
2023-03-10 22:36:46 +0530

    Added code

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+import ChannelCard from './ChannelCard';
+
+const Home = () => {
+  return (
+    <View style={{flex: 1}}>
+        <ChannelCard />
+    </View>
+  );
+};
+
+export default Home;
